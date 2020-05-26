4e5d8e9dee39fa0c38e8b9f8a2cd75f2c62a1c3e
Nikita Andrasovich <44532175+HappyGnom@users.noreply.github.com>
2020-05-26 16:00:32 +0300

    Update Task3.py
    Refactoring

--- Task3.py
+++ Task3.py
@@ -5,28 +5,30 @@ import numpy as np
 
 
 def draw_histogram(values):
-    values.sort()
+    sorted_values = sorted(values)
+    length = len(sorted_values)
+
     table = PrettyTable()
     table.field_names = ["Start", "End", "Height"]
 
-    columns = int(np.sqrt(len(values)))
-    values_per_column = int(len(values)/columns)
+    columns = int(np.sqrt(length))
+    values_per_column = int(length/columns)
 
-    A = [values[0]]
-    B = [(values[values_per_column-1]+values[values_per_column])/2]
+    A = [sorted_values[0]]
+    B = [(sorted_values[values_per_column-1]+sorted_values[values_per_column])/2]
 
     for i in range(1, columns):
         A.append(B[i-1])
 
-        if (i+1)*(values_per_column) >= len(values):
-            B.append(values[len(values)-1])
+        if (i+1)*(values_per_column) >= length:
+            B.append(sorted_values[length-1])
         else:
-            B.append((values[(i+1)*(values_per_column-1)] +
-                      values[(i+1)*(values_per_column)])/2)
+            B.append((sorted_values[(i+1)*(values_per_column-1)] +
+                      sorted_values[(i+1)*(values_per_column)])/2)
 
     for i in range(columns):
         width = B[i] - A[i]
-        height = values_per_column/(len(values)*width)
+        height = values_per_column/(length*width)
 
         table.add_row([A[i], B[i], height])
         plt.bar(x=A[i], height=height, width=width, align='edge')
@@ -34,7 +36,6 @@ def draw_histogram(values):
     plt.title('Distribution Histogram')
     plt.xlabel('Value')
     plt.ylabel('Possibility')
-
     plt.ylim(0, 3)
     plt.grid(axis='y', alpha=0.8)
 
@@ -43,13 +44,13 @@ def draw_histogram(values):
 
 
 def draw_polygon(values):
-    values.sort()
-    # columns = int(np.sqrt(len(values)))
-    columns = 8
-    column_range = 4/columns
+    sorted_values = sorted(values)
 
     table = PrettyTable()
     table.field_names = ["From", "To", "Count"]
+
+    columns = 8
+    column_range = 4/columns
 
     points = {}
 
@@ -58,7 +59,7 @@ def draw_polygon(values):
         end = (i+1) * column_range
         range_middle = end - column_range/2
 
-        count = sum(start < x <= end for x in values)
+        count = sum(start < x <= end for x in sorted_values)
 
         table.add_row([start, end, count])
         points[range_middle] = count
@@ -78,27 +79,30 @@ def draw_polygon(values):
 
 
 def draw_distribution_function(values):
-    values.sort()
-    F = []
+    sorted_values = sorted(values)
+    length = len(sorted_values)
 
     table = PrettyTable()
     table.field_names = ["x", "F*(x)"]
 
-    for i in range(len(values)):
-        function = i/len(values)
+    F = []
+
+    for i in range(length):
+        function = i/length
         F.append(function)
         table.add_row([i, function])
 
-    plt.title('Distribution Function for '+str(len(values)) + ' values')
+    plt.title('Distribution Function for '+str(length) + ' values')
     plt.xlabel('Value')
     plt.ylabel('F*(x)')
     plt.grid(axis='y', alpha=0.8)
 
-    plt.plot(values, F)
+    plt.plot(sorted_values, F)
+
     print(table.get_string(title="Distr. F* Table"))
     plt.show()
 
 
 draw_histogram(get_random_sequence(100))
 draw_polygon(get_random_sequence(1000))
-draw_distribution_function(get_random_sequence(500))
+draw_distribution_function(get_random_sequence(1000))
